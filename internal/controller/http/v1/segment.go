65776fb5db261ba56fcf98546ee095b9aa8ae223
student31415 <qwerty65k@mail.ru>
2023-10-18 23:18:46 +0300

    change error handling

--- internal/controller/http/v1/segment.go
+++ internal/controller/http/v1/segment.go
@@ -1,16 +1,19 @@
 package v1
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/GOodCoffeeLover/avito-backend-trainee-assignment-2023/internal/entity"
 	"github.com/GOodCoffeeLover/avito-backend-trainee-assignment-2023/internal/usecase/segment"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 type errorResponse struct {
 	Error string `json:"error"`
+	Code  int    `json:"code"`
 }
 
 const (
@@ -24,9 +27,10 @@ type segmentRoutes struct {
 func initSegmentRoutes(handler *gin.RouterGroup, segments segment.Segments) {
 	sr := segmentRoutes{segmets: segments}
 	h := handler.Group("/segments")
-	h.GET(fmt.Sprintf("/:%v", segmentNameParam), sr.read)
 
 	h.GET("/", sr.readAll)
+	h.GET(fmt.Sprintf("/:%v", segmentNameParam), sr.read)
+
 	h.POST(fmt.Sprintf("/:%v", segmentNameParam), sr.create)
 	h.DELETE(fmt.Sprintf("/:%v", segmentNameParam), sr.delete)
 }
@@ -36,7 +40,7 @@ func (sr *segmentRoutes) read(ctx *gin.Context) {
 
 	segment, err := sr.segmets.Read(ctx.Request.Context(), entity.SegmentName(name))
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse{err.Error()})
+		abortWithErrorAnalize(ctx, err)
 		return
 	}
 	ctx.JSON(http.StatusOK, segment)
@@ -47,7 +51,7 @@ func (sr *segmentRoutes) readAll(ctx *gin.Context) {
 
 	segments, err := sr.segmets.ReadAll(ctx.Request.Context())
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse{err.Error()})
+		abortWithErrorAnalize(ctx, err)
 		return
 	}
 	ctx.JSON(http.StatusOK, segments)
@@ -58,7 +62,7 @@ func (sr *segmentRoutes) create(ctx *gin.Context) {
 	name := ctx.Param(segmentNameParam)
 
 	if err := sr.segmets.Create(ctx.Request.Context(), entity.SegmentName(name)); err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse{err.Error()})
+		abortWithErrorAnalize(ctx, err)
 		return
 	}
 	ctx.Status(http.StatusCreated)
@@ -67,11 +71,20 @@ func (sr *segmentRoutes) create(ctx *gin.Context) {
 func (sr *segmentRoutes) delete(ctx *gin.Context) {
 	name := ctx.Param(segmentNameParam)
 
-	err := sr.segmets.Delete(ctx.Request.Context(), entity.SegmentName(name))
-	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, errorResponse{err.Error()})
+	if err := sr.segmets.Delete(ctx.Request.Context(), entity.SegmentName(name)); err != nil {
+		abortWithErrorAnalize(ctx, err)
 		return
 	}
 	ctx.Status(http.StatusOK)
 
+}
+
+func abortWithErrorAnalize(ctx *gin.Context, err error) {
+	status := http.StatusInternalServerError
+	switch {
+	case errors.Is(err, pgx.ErrNoRows):
+		status = http.StatusNotFound
+
+	}
+	ctx.AbortWithStatusJSON(status, errorResponse{err.Error(), status})
 }
