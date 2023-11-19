2be9f1eced5888cab513ad0e652ded59503675e4
student31415 <qwerty65k@mail.ru>
2023-11-19 22:16:43 +0300

    fix nginx wait

--- tools/scripts/install-ingress.sh
+++ tools/scripts/install-ingress.sh
@@ -1,8 +1,7 @@
 set -uex -o pipefail
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-sleep 1
-kubectl wait --namespace ingress-nginx \
+while !(kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
+  --timeout=90s); do sleep 1; done
